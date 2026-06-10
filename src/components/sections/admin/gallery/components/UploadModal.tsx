35b6e154e4e112fb3e32b56f8eb8bc66240763dd
chetan <chetanchoudhary435@gmail.com>
2026-06-10 18:20:27 +0530

    admin gallary section

--- src/components/sections/admin/gallery/components/UploadModal.tsx
+++ src/components/sections/admin/gallery/components/UploadModal.tsx
@@ -470,7 +470,7 @@ export default function UploadModal({ onClose, onSuccess }: UploadModalProps) {
               </div>
               <div className="h-2 bg-gray-100 rounded-full overflow-hidden">
                 <div
-                  className="h-full bg-gradient-to-r from-teal-400 to-teal-600 rounded-full transition-all duration-300"
+                  className="h-full bg-linear-to-r from-teal-400 to-teal-600 rounded-full transition-all duration-300"
                   style={{ width: `${progress}%` }}
                 />
               </div>
